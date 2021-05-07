805f9cddc5bd2d06b64d8e371b68b06453dec7e1
sita021 <jsitaric@gmail.com>
2021-05-07 04:54:11 +0200

    Implemented modal  price logic

--- app/js/modal.js
+++ app/js/modal.js
@@ -17,7 +17,7 @@ var modal = null;
 
 btn.on("click", function () {
     // Get the current modal
-    // When the user clicks on the button, open the modal
+    // When the user clicks on the button, open the unique modal
     modal = $(".products-section__modal").eq(button);
     modal.show();
 });
@@ -26,4 +26,68 @@ btn.on("click", function () {
 span.on("click", function () {
     // When the user clicks on the button, close the modal
     modal.hide();
+});
+
+/**
+ * Checkbox modal calculations
+ */
+
+var totalPrice = 0;
+var newPrice = 0;
+var value = 0;
+var checkboxes = $(".products-section__modal-info-checkbox");
+var currentModal = null;
+var priceArray = [];
+
+// Get total price based on unique modal
+btn.click(function () {
+    currentModal = $(".products-section__modal-button-price").eq(button);
+    console.log(currentModal);
+    totalPrice = $(".products-section__modal-button-price")
+        .eq(button)
+        .attr("data-price");
+    console.log(totalPrice);
+    console.log(typeof totalPrice);
+    // Current modal
+    console.log(modal);
+    $(modal)
+        .find(".products-section__modal-info-checkbox:checked")
+        .each(function () {
+            // Creating an array of string values
+            priceArray.push($(this).attr("value"));
+            console.log(priceArray);
+        });
+
+    // Sum array of values
+    var sum = 0;
+    console.log(typeof sum);
+    for (var i = 0; i < priceArray.length; i++) {
+        sum += Math.round((priceArray[i] * 100) / 100);
+    }
+
+    newPrice = +totalPrice + sum;
+    newPrice += "";
+
+    console.log(sum);
+    console.log(typeof sum);
+
+    console.log(totalPrice);
+    console.log(typeof totalPrice);
+
+    console.log(newPrice);
+    console.log(typeof newPrice);
+
+    currentModal.html((newPrice + "&#8364;"));
+});
+
+// Get checkbox value if clicked
+$(checkboxes).click(function () {
+    if ($(this).is(":checked")) {
+        value = $(this).attr("value");        
+        // Calculations...
+        console.log(currentModal.html((newPrice = value + "&#8364;")));
+        console.log(value);
+    } else {
+        console.log(currentModal.html(totalPrice + "&#8364;"));
+    }
 });
